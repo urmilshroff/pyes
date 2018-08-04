0f6868677c0b9652e59c5ab8054f7e98e53b7bbc
urmilshroff <urmilshroff@gmail.com>
2018-08-04 23:41:06 +0530

    Updated

--- pyes.py
+++ pyes.py
@@ -1,14 +1,24 @@
-import cv2
-import numpy as np
+import sys
 
-cap=cv2.VideoCapture(0) #0 is id of webcam
+try:
+    import cv2
+    import numpy as np
+    dependencies=True
+    
+except ImportError:
+    print("\nSorry, you don't seem to have OpenCV installed for the following version of Python: {}\n\nYou can try by running 'pip3 install opencv-python'.\n".format(sys.version))
+    dependencies=False
+    
+if dependencies==True:
+    
+    cap=cv2.VideoCapture(0) #0 is id of webcam
 
-while True:
-    ret, frame=cap.read() #returns each frame of the video
-    gray_frame=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY) #converts each frame to grayscale
+    while True:
+        ret, frame=cap.read() #returns each frame of the video
+        gray_frame=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY) #converts each frame to grayscale
 
-    cv2.imshow("Color video",frame)
-    cv2.imshow("Gray video",gray_frame)
+        cv2.imshow("Color video",frame)
+        cv2.imshow("Gray video",gray_frame)
 
-    if cv2.waitKey(1) and 0xFF==ord("q"): #pressing "q" kills the video
-        break
+        if cv2.waitKey(1) and 0xFF==ord("q"): #pressing "q" kills the video
+            break
