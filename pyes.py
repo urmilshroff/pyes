a2d02915faf365bcf13b489d05c67959894d2a82
urmilshroff <urmilshroff@gmail.com>
2018-09-06 16:59:04 +0530

    FFFF

--- pyes.py
+++ pyes.py
@@ -162,3 +162,27 @@
     # ipApp=PyesApp()
     # ipApp.run()
     
+import kivy
+kivy.require("1.10.0") #not important
+
+from kivy.app import App #classes
+from kivy.uix.gridlayout import GridLayout
+
+class CalcGridLayout(GridLayout):
+    def calculate(self,args):
+        if args:
+            try:
+                self.input.text=str(eval(args))
+            except Exception:
+                self.input.text="Error!"
+                
+    def clear(self,args):
+        if args:
+            self.input.text=("")                
+    
+class CalculatorApp(App):
+    def build(self):
+        return CalcGridLayout()
+        
+calcApp=CalculatorApp()
+calcApp.run()
